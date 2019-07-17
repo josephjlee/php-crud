c2bc8302e45b1b75cf0fefa81794444324dbfb1f
Raphael Cabral <brcmesquita@gmail.com>
2019-07-17 09:34:22 -0300

    Projeto finalizado

--- index.php
+++ index.php
@@ -3,75 +3,90 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP CRUD</title>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <title>PHP CRUD</title>
 </head>
 
 <body>
     <?php require_once 'process.php'; ?>
+
+    <?php if (isset($_SESSION['message'])): ?>
+
+        <div class="alert alert-<?=$_SESSION['msg_type']?>">
+            <?php
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+            ?>
+        </div>
+    <?php endif ?>
+
     <div class="container">
-    <?php
+        <?php
         $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
         $result = $mysqli->query("SELECT * FROM data");
-    ?>
+        ?>
 
-    <div class="row justify-content-center">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Location</th>
-                    <th colspan="2">Action</th>
-                </tr>
-            </thead>
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Location</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
 
-    <?php
-        while ($row = $result->fetch_assoc()): ?>
-        <tr>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['location']; ?></td>
-            <td>
-                <a href="index.php?edit=<?php echo $row['id']; ?>"
-                    class="btn btn-info">Edit</a>
-                <a href="index.php?delete=<?php echo $row['id']; ?>"
-                    class="btn btn-danger">Delete</a>
-            </td>
-        </tr>
-    <?php endwhile; ?>
+                <?php while ($row = $result->fetch_assoc()): ?>
+                    <tr>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><?php echo $row['location']; ?></td>
+                        <td>
+                            <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-outline-info btn-sm">Edit</a>
+                            <a href="process.php?delete=<?php echo $row['id'];?>" class="btn btn-outline-danger btn-sm" name="delete" id="delete" onclick="return confirm('Are you sure you want to delete this item?');">Delete</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
 
-        </table>
+            </table>
+        </div>
+
+        <?php
+
+        function pre_r($array)
+        {
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        }
+
+        ?>
+
+        <div class="row justify-content-center">
+            <form action="process.php" method="POST">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <div class="form-group">
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" value="<?php echo $name; ?>" placeholder="Enter your name">
+                </div>
+                <div class="form-group">
+                    <label>Location</label>
+                    <input type="text" name="location" value="<?php echo $location; ?>" class="form-control" placeholder="Enter your location">
+                </div>
+                <div class="form-group">
+                    <?php
+                    if ($update == true) :
+                        ?>
+                        <button type="submit" class="btn btn-info" name="update" onclick="return confirm('Are you sure you want to delete this item?');">Update</button>
+                    <?php else : ?>
+                        <button type="submit" class="btn btn-primary" name="save">Save</button>
+                    <?php endif; ?>
+                </div>
+            </form>
+        </div>
     </div>
 
-    <?php
-
-function pre_r( $array ) {
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
-}
-
-    ?>
-
-    <div class="row justify-content-center">
-        <form action="process.php" method="post">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" name="name" class="form-control" value="Enter your name">
-            </div>
-            <div class="form-group">
-                <label>Location</label>
-                <input type="text" name="location" class="form-control" value="Enter your location">
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary" name="save">Save</button>
-            </div>
-        </form>
-    </div>
-    </div>
 
 </body>
 
