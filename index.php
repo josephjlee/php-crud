0e3d6ee6810c186396d47ff2f6d47f31512b12ff
Raphael Cabral <brcmesquita@gmail.com>
2019-07-16 22:15:18 -0300

    update index.php

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>PHP CRUD</title>
 </head>
+
 <body>
+    <?php require_once 'process.php'; ?>
     <div class="row justify-content-center">
-    <form action="" method="post">
-        <div class="form-group">
-        <label>Name</label>
-        <input type="text" name="name" class="form-control" value="Enter your name">
-        </div>
-        <div class="form-group">
-        <label>Location</label>
-        <input type="text" name="location" class="form-control" value="Enter your location">
-        </div>
-        <div class="form-group">
-        <button type="submit" class="btn btn-primary" name="save">Save</button>
-        </div>
-    </form>
+        <form action="process.php" method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control" value="Enter your name">
+            </div>
+            <div class="form-group">
+                <label>Location</label>
+                <input type="text" name="location" class="form-control" value="Enter your location">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" name="save">Save</button>
+            </div>
+        </form>
     </div>
-    
-</body> 
+
+</body>
+
 </html>
